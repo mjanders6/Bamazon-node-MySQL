16ddc663fcd5f80fa4cae5ea3a3fdea0e36c93ef
Michael Anderson <mjanders6@gmail.com>
2019-04-28 14:09:57 -0700

    destructured product columns

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,23 +4,29 @@ const inquirer = require('inquirer')
 
 // create connection 
 const db = createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: process.env.BAMAZON_HOST,
+    user: process.env.BAMAZON_USER,
     password: process.env.BAMAZON_SECRET,
-    database: 'bamazon'
+    database: process.env.BAMAZON_DB
 })
 
-db.query(`SELECT * FROM products`, (error, results) => {
-    if (error) { console.log(error) } else {
-        // console.log(results)
-        results.forEach((items) => {
-            console.log(`
-                Name: ${items.product_name}
-                Dept. Name: ${items.department_name}
-                Price: $${items.price}
-            `)
+db.connect(e => {
+    if (e) { console.log(e) } else {
+        db.query(`SELECT * FROM products`, (error, results) => {
+            if (error) { console.log(error) } else {
+                // console.log(results)
+                results.forEach((items) => {
+                    let {item_id, product_name, department_name, price, stock_quantity} = items
+                    console.log(`${item_id} ${product_name} $${price} each (${stock_quantity} left in stock)`)
+                    // console.log(`
+                    //     ID: ${items.item_id}
+                    //     Name: ${items.product_name}
+                    //     Dept. Name: ${items.department_name}
+                    //     Price: $${items.price}
+                    // `)
+                })
+                process.exit()
+            }
         })
-
-        process.exit()
     }
 })
