826fabdfee3433b6e69665ff57f612fd3e9ab6a3
Michael Anderson <mjanders6@gmail.com>
2019-04-28 14:00:05 -0700

    SELECT and log from products table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,10 +2,25 @@ require('dotenv').config()
 const { createConnection } = require('mysql2')
 const inquirer = require('inquirer')
 
+// create connection 
 const db = createConnection({
     host: 'localhost',
     user: 'root',
     password: process.env.BAMAZON_SECRET,
-    database: 'greatBay_DB'
+    database: 'bamazon'
 })
 
+db.query(`SELECT * FROM products`, (error, results) => {
+    if (error) { console.log(error) } else {
+        // console.log(results)
+        results.forEach((items) => {
+            console.log(`
+                Name: ${items.product_name}
+                Dept. Name: ${items.department_name}
+                Price: $${items.price}
+            `)
+        })
+
+        process.exit()
+    }
+})
