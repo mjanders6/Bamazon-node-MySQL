c5bea00e4315df1ec06f7cec9411d746f180448f
Michael Anderson <mjanders6@gmail.com>
2019-04-28 22:28:42 -0700

    username check answer form productView

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const { createConnection } = require('mysql2')
 const inquirer = require('inquirer')
 
+let username 
+
 // create connection 
 const db = createConnection({
     host: process.env.BAMAZON_HOST,
@@ -16,13 +18,16 @@ db.connect(e => {
     }
 })
 
-
-
 let productView = _ => {
     db.query(`SELECT * FROM products`, (error, results) => {
         if (error) { console.log(error) } else {
             inquirer
                 .prompt([
+                    {
+                        name: 'username',
+                        type: 'input',
+                        message: 'What is your name'
+                    },
                     {
                         name: 'choice',
                         type: 'rawlist',
@@ -41,7 +46,11 @@ let productView = _ => {
                         type: 'input',
                         message: 'How many do you want to buy?'
                     }
-                ])
+                ]).then((answer) => {
+                    console.log(answer)
+                    // process.exit()
+
+                })
             // let prodList = []
             // results.forEach((items) => {
             //     let { item_id, product_name, department_name, price, stock_quantity } = items
@@ -52,4 +61,5 @@ let productView = _ => {
             // console.log(prodList)
         }
     })
+
 }
