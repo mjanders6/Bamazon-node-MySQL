f1ddbfd3438d9f2b2c72f693254f6d34c0ac5b26
Michael Anderson <mjanders6@gmail.com>
2019-05-04 23:22:13 -0700

    include sql file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,6 +26,19 @@ async function prodView(column) {
     return response
 }
 
+const purchView = _ => {
+    prodView('*')
+        .then(r => {
+
+            r.forEach(({ item_id, product_name, price, stock_quantity }) => {
+                console.log(`
+                ${item_id}) ${product_name} ($${price} each) (${stock_quantity} left in stock)
+                `)
+            })
+        })
+        .catch(e => console.log(e))
+}
+
 // userName
 const userName = _ => {
     prompt({
@@ -34,8 +47,9 @@ const userName = _ => {
         message: 'Please register your name:',
     })
         .then(({ uName }) => {
-            console.log(uName)
-            process.exit()
+            username = uName
+            addToCart()
+            // process.exit()
         })
         .catch(e => console.log(e))
 }
@@ -46,7 +60,8 @@ const userName = _ => {
 const addToCart = _ => {
     prodView('*')
         .then(r => {
-            console.log(r.map(({ product_name }) => `${product_name}`))
+            console.log(r.map(({ item_id, product_name, price, stock_quantity }) => `${item_id}) ${product_name} ($${price} each) (${stock_quantity} left in stock)`))
+
             prompt([{
                 type: 'rawlist',
                 name: 'product_name',
@@ -66,33 +81,41 @@ const addToCart = _ => {
                         }
                     })
                     cart.totalCost = response.selQTY * cart.price
-                    console.log(cart)
-                    db.query(`INSERT INTO shopping_cart SET ?`, {
-                        user_name: 'mike',
-                        product_name: cart.product_name,
-                        department_name: cart.department_name,
-                        price: cart.price,
-                        purchase_quantity: response.selQTY,
-                        total_cost: cart.totalCost
-                    }, (err) => {
-                        if (err) throw err
-                        console.log('Your auction was created successfully!')
-                        openBamazon()
-                    })
+                    // console.log(cart)
 
-                    db.query(`UPDATE products SET ? WHERE ?`, 
-                    [
-                        {
-                            stock_quantity: cart.stock_quantity - response.selQTY
-                        },
-                        {
-                            item_id: cart.item_id
-                        }
-                    ], (err) => {
-                        if (err) throw err
-                        console.log('Your auction was successfully updated!')
+                    if (response.selQTY > cart.stock_quantity) {
+                        console.log('not enough stock')
+                        addToCart()
+                    } else {
+                        console.log('thank you')
+                        db.query(`INSERT INTO shopping_cart SET ?`, {
+                            user_name: username,
+                            product_name: cart.product_name,
+                            department_name: cart.department_name,
+                            price: cart.price,
+                            purchase_quantity: response.selQTY,
+                            total_cost: cart.totalCost
+                        }, (err) => {
+                            if (err) throw err
+                            console.log('Added to shopping cart!')
+                            // addToCart()
+                        })
+
+                        db.query(`UPDATE products SET ? WHERE ?`,
+                            [
+                                {
+                                    stock_quantity: cart.stock_quantity - response.selQTY
+                                },
+                                {
+                                    item_id: cart.item_id
+                                }
+                            ], (err) => {
+                                if (err) throw err
+                                // console.log('added')
+                                // addToCart()
+                            })
                         openBamazon()
-                    })
+                    }
                 })
                 .catch(e => console.log(e))
 
@@ -112,38 +135,6 @@ const addToCart = _ => {
 // show items in shopping cart, select one to remove, remove it from the shopping cart, add it back to products DB, 
 // prompt to check out, remove items, choose more items, exit
 
-// let productView = _ => {
-//     db.query(`SELECT * FROM products`, (error, results) => {
-//         if (error) { console.log(error) } else {
-//             inquirer
-//                 .prompt([
-//                     {
-//                         name: 'choice',
-//                         type: 'rawlist',
-//                         choices: function () {
-//                             let prodList = []
-//                             results.forEach((items) => {
-//                                 let { item_id, product_name, department_name, price, stock_quantity } = items
-//                                 prodList.push(`${item_id} ${product_name} $${price} each (${stock_quantity} left in stock)`)
-//                             })
-//                             return prodList
-//                         },
-//                         message: 'Choose something to buy'
-//                     },
-//                     {
-//                         name: 'qtyBuy',
-//                         type: 'input',
-//                         message: 'How many do you want to buy?'
-//                     }
-//                 ]).then((answer) => {
-//                     // process.exit()
-//                     productView()
-//                 })
-//         }
-//     })
-
-// }
-
 const openBamazon = _ => {
     prompt({
         type: 'list',
@@ -154,7 +145,7 @@ const openBamazon = _ => {
         .then(({ prodshow }) => {
             switch (prodshow) {
                 case 'Product View / Go Shopping':
-                    addToCart()
+                    (!username) ? userName() : addToCart()
                     break;
 
                 case 'Check Shopping Cart':
@@ -180,5 +171,6 @@ const openBamazon = _ => {
 db.connect(e => {
     if (e) { console.log(e) } else {
         openBamazon()
+        // purchView()
     }
 })
